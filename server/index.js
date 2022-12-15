15a6fbd6e63e702784b72ce5d21a2a918b345b99
SamuelIgwesi <samhillss231@gmail.com>
2022-12-15 15:06:03 +0100

    Changed path.join to path.resolve

--- server/index.js
+++ server/index.js
@@ -6,11 +6,14 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "client/pages/checkout.jsx")));
+app.use(
+  express.static(path.resolve(__dirname, "../client/pages/checkout.jsx"))
+);
 
 app.post("/cart", (req, res) => {
+  let {} = req.body;
   if (!req.body) {
-    return res.sendStatus(400);
+    res.sendStatus(400);
   } else {
     res.sendStatus(200);
   }
