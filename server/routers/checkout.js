2d4f4d436e7000d93bf77cb7f92d9a59cb513363
SamuelIgwesi <samhillss231@gmail.com>
2022-12-17 12:56:48 +0100

    Modified static file

--- server/routers/checkout.js
+++ server/routers/checkout.js
@@ -4,17 +4,19 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.urlencoded({ extended: true }));
 router.use(
-  express.static(path.resolve(__dirname, ".././client/pages/checkout.jsx"))
+  express.static(
+    path.resolve(__dirname, "../../client/src/Pages/Checkout/Checkout.jsx")
+  )
 );
 
-router.post("/cart", (req, res) => {
-  let {} = req.body;
+router.post("/checkout", (req, res) => {
   if (!req.body) {
     res.sendStatus(400);
   } else {
     res.sendStatus(200);
+    console.log(req.body.post);
   }
 });
 
