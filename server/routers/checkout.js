46542bcdccf56cbd0878e0845b8af0fc3fb5dd3f
SamuelIgwesi <samhillss231@gmail.com>
2023-01-22 01:54:58 +0100

    Updated checkout.js

--- server/routers/checkout.js
+++ server/routers/checkout.js
@@ -4,32 +4,30 @@ const cors = require("cors");
 
 const formData = require("../models/checkout.js");
 
-router.use(cors());
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
-router.use((req, res, next) => {
-  // Change CORS Origin on deployment
-  res.setHeader("Access-Control-Allow-Origin", "http:localhost:3000");
+const whitelist = ["http://localhost:3000"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+router.use(cors(corsOptions));
 
-  res.setHeader("Access-Control-Allow-Methods", "POST");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  next();
-});
 router.post("/api/checkout", cors(), async (req, res) => {
   if (!req.body) {
     res.sendStatus(400);
   } else {
     res.sendStatus(200);
-    console.log("connected");
-    const vals = Object.keys(req.body).map(function (key) {
-      return req.body[key];
-    });
+
+    const vals = Object.entries(req.body).map();
+    return vals;
   }
 });
 
