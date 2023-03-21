640a24b46f88b41b3ba6456be9f4d795fe32a8a4
SamuelIgwesi <samhillss231@gmail.com>
2023-03-21 17:57:09 +0100

    Refactored DB connection code

--- server/routers/checkout.js
+++ server/routers/checkout.js
@@ -3,26 +3,21 @@ const router = require("express").Router();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const checkoutData = require("../models/checkout.js");
 
 require("dotenv").config();
 
-// CONNECT TO MONGOOSE TO MONGODB
+// CONNECTION TO DATABASE
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(process.env.MONGO_URL);
+  mongoose.set("strictQuery", true);
+  await mongoose.connect(
+    process.env.MONGO_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log("Connected to DB")
+  );
 }
-
-mongoose.set("strictQuery", true);
-
-// Connect to DB
-mongoose.connect(
-  process.env.MONGO_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => console.log("Connected to DB")
-);
-
-const checkoutData = require("../models/checkout.js");
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
@@ -44,7 +39,6 @@ router.use(cors());
 // Get the client data on submit
 router.post("/api/checkout", async (request, response) => {
   const user = new checkoutData(request.body);
-  // console.log(request.body);
 
   try {
     await user.save();
